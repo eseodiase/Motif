30eda1a965c7f8ec355ff6c326f541e0df0b5bff
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-19 19:56:36 +0100

    Add files via upload

---
+++ components/about.js
@@ -0,0 +1,19 @@
+const about =()=>{
+   return `<section class="aboutPage">
+<div class="aboutArticle">
+<img src="#" alt="aboutUsImg" class="aboutUsImg" 
+width="100%" height="300px"/>
+<article>
+<h3>About Us</h3>
+<p> 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque posuere, ligula in finibus semper, nisi odio lobortis libero, eu aliquet diam massa vitae ipsum. Cras laoreet tortor a mi gravida, eu vestibulum metus venenatis. Aenean maximus lacus quis neque mollis accumsan. Fusce finibus aliquam purus id malesuada.
+</p>
+
+<p>
+massa vitae ipsum. Cras laoreet tortor a mi gravida, eu vestibulum metus venenatis. Aenean maximus lacus quis neque mollis accumsan. Fusce finibus aliquam purus id malesuada.
+</p>
+</article>
+</div>
+          </section>`; 
+}
+export default about;
