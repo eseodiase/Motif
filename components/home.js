f891468b5060d18dfc68b4dcfd8b32d58b49d43b
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-20 14:06:27 +0100

    home.js

--- components/home.js
+++ components/home.js
@@ -45,7 +45,7 @@ return `<section class="homePage">
             <div class="content2">
             <div class="subContent2">
    <h3>${homeObj.subTitle}</h3>
-   <iframe src="components/project/project.html" class="nftAction"></iframe>
+   <iframe src="https://eseodiase.github.io/Motif/components/project/project.html" class="nftAction"></iframe>
    <p>« Explore more projects »</p>
             </div>
             </div>
